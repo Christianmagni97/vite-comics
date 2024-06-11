92f1511f6d80c86a1434e70b99c778e8fcb5072c
Christianmagni97 <christianmagni28@gmail.com>
2024-06-12 01:15:46 +0200

    aggiunto varianti

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,8 +2,37 @@
 
 </script>
 <template>
+    <header>
+        <section id="logo">
+            <img src="../assets/img/dc-logo.png" alt="dc logo">
+        </section>
+        <section id="navBar">
+            <nav>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li class="active">COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>CHARACTERS</li>
+                </ul>
+            </nav>
+
+        </section>
+    </header>
   
 </template>
-<style>
+<style lang="scss">
+@use '../assets/style/partials/variabls.scss' as *;
+ul{
+    display: flex;
 
+}
+li{
+    margin-right: 1rem;
+    &.active{
+        color: $color-second-main;
+        border-bottom: $color-second-main;
+    }
+}
 </style>
